746404712bf4d85097d47fd280e691f215a51081
mxq <chandler2code@foxmail.com>
2018-06-08 11:35:14 +0800

    登录操作（二）

--- create_table.php
+++ create_table.php
@@ -5,12 +5,21 @@
  * Date: 2018/4/27
  * Time: 13:56
  */
+// require_once("db_config.php");
+// mysqli_query($link,"drop database student");
+// mysqli_query($link,"create database student");
+// mysqli_query($link,"use student");
+// mysqli_query($link,"create table user(name CHAR(50) ,password CHAR(40) engine=myisam DEFAULT charset=utf8");
+// mysqli_query($link,"insert into user(name,password) VALUES ('mike','1')");
+// mysqli_query($link,"insert into user(name,password) VALUES ('mary','2')");
+// echo mysqli_error($link);
+
 require_once("db_config.php");
 mysqli_query($link,"drop database student");
 mysqli_query($link,"create database student");
 mysqli_query($link,"use student");
-mysqli_query($link,"create table stuinfo(id CHAR(50),name CHAR (50) ,score INT )engine=myisam DEFAULT charset=utf8");
-mysqli_query($link,"insert into stuinfo(id,name,score)VALUES ('001','mike','78')");
-mysqli_query($link,"insert into stuinfo(id,name,score)VALUES ('002','mary','88')");
+mysqli_query($link,"create table user(name char(50),password char(50))ENGINE=MyISAM DEFAULT CHARSET=utf8");
+mysqli_query($link,"insert into user(name,password)values('mike','11')");
+mysqli_query($link,"insert into user(name,password)values('rose','11')");
 echo mysqli_error($link);
 
