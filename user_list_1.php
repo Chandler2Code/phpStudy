aac08005cdfb6d35da96602da8dfe2f9efdca31c
mxq <chandler2code@foxmail.com>
2018-06-08 12:29:13 +0800

    0608学习

--- user_list_1.php
+++ user_list_1.php
@@ -9,5 +9,11 @@ if(isset($_SESSION['name'])){
 }else{
     echo "无权访问";
     echo "<a href='login_1.php'>login</a>";
+
+    require_once("db_config.php");
+    $query_string = "select * from user";
+    $rs = mysqli_query($link,$query_string);
+    $row = 
+    while (E\)
 }
 
